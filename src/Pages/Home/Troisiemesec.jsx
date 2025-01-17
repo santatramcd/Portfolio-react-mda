e8a8df93243ed40e2a41f4f638ad40f45aea7d75
santatramcd <andrianjakasantatra22@gmail.com>
2025-01-17 10:20:49 +0300

    add the categ btn portfolio

--- src/Pages/Home/Troisiemesec.jsx
+++ src/Pages/Home/Troisiemesec.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function About() {
+export default function Home() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [selectedItem, setSelectedItem] = useState(null); // État pour gérer l'élément sélectionné
+  const [selectedItem, setSelectedItem] = useState(null); // Élément sélectionné pour le modal
+  const [filter, setFilter] = useState("*"); // État pour gérer le filtre sélectionné
 
   const baseID = "appTVkIAf30WtN760";
   const tableName = "Work";
@@ -26,6 +27,7 @@ export default function About() {
         id: record.id,
         title: record.fields.Name || "Sans titre",
         techno: record.fields.Technology || "Sans description",
+        category: record.fields.Category || "Autre", // Catégorie ajoutée
         img: record.fields.Photos?.[0]?.url || "",
         link: record.fields.Link || "#",
       }));
@@ -43,119 +45,185 @@ export default function About() {
     fetchAirtableData();
   }, []);
 
+  // Fonction pour filtrer les données
+  const filteredData =
+    filter === "*"
+      ? data
+      : data.filter((item) =>
+          item.techno.toLowerCase().includes(filter.toLowerCase())
+        );
+
   return (
-    <div>
-      <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">Portfolio</h2>
-            <h3 className="section-subheading text-muted">
-              Découvrez mes projets récents.
-            </h3>
+    <>
+      <div>
+        <section id="portfolio" className="portfolio section page-section">
+          <div className="container" data-aos="fade-up">
+            <div className="text-center">
+              <h2 className="section-heading text-uppercase">Portfolio</h2>
+              <h3 className="section-subheading text-muted">
+                Découvrez mes projets récents.
+              </h3>
+            </div>
           </div>
 
-          {loading && (
-            <div class="d-flex justify-content-center">
-              <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-              </div>
-            </div>
-          )}
-          {error && <p>{error}</p>}
-
-          {!loading && !error && (
-            <div className="row">
-              {data.map((item) => (
-                <div key={item.id} className="col-lg-4 col-sm-6 mb-4">
-                  <div className="portfolio-item">
-                    <a className="portfolio-link" href="#">
-                      <div className="portfolio-hover">
-                        <div className="portfolio-hover-content">
-                          <a
-                            href="#"
-                            data-bs-toggle="modal"
-                            data-bs-target="#exampleModal"
-                            onClick={() => setSelectedItem(item)} // Définit l'élément sélectionné
-                          >
-                            <i className="bi bi-plus fa-3x"></i>
-                          </a>
-                          <a
-                            href={item.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <i className="bi bi-link-45deg fa-2x"></i>
-                          </a>
-                        </div>
-                      </div>
-                      <img
-                        className="img-fluid img-portfolio"
-                        src={item.img}
-                        alt={item.title}
-                      />
-                    </a>
-                    <div className="portfolio-caption">
-                      <div className="portfolio-caption-heading">
-                        {item.title}
-                      </div>
-                      <div className="portfolio-caption-subheading text-muted">
-                        {item.techno}
-                      </div>
-                    </div>
+          <div className="container">
+            <div
+              className="isotope-layout portfolio"
+              data-default-filter="*"
+              data-layout="masonry"
+              data-sort="original-order"
+            >
+              <ul
+                className="portfolio-filters isotope-filters"
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <li
+                  className={` ${filter === "*" ? "active" : ""}`}
+                  onClick={() => setFilter("*")}
+                >
+                  All
+                </li>
+                <li
+                  className={filter === "WordPress" ? "active" : ""}
+                  onClick={() => setFilter("WordPress")}
+                >
+                  WordPress
+                </li>
+                <li
+                  className={filter === "Vuejs 3" ? "active" : ""}
+                  onClick={() => setFilter("Vuejs 3")}
+                >
+                  Vuejs 3
+                </li>
+                <li
+                  className={filter === "React Js" ? "active" : ""}
+                  onClick={() => setFilter("React Js")}
+                >
+                  React Js
+                </li>
+                <li
+                  className={filter === "Application" ? "active" : ""}
+                  onClick={() => setFilter("Application")}
+                >
+                  App
+                </li>
+                <li
+                  className={filter === "grav cms" ? "active" : ""}
+                  onClick={() => setFilter("grav cms")}
+                >
+                  Grav cms
+                </li>
+              </ul>
+              {loading && (
+                <div className="d-flex justify-content-center">
+                  <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
                   </div>
                 </div>
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+              )}
+              {error && <p>{error}</p>}
 
-      {/* Modal */}
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-fullscreen">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                {selectedItem?.title || "Détails"}
-              </h5>
-              <a
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              >
-                <i class="bi bi-x-lg"></i>
-              </a>
-            </div>
-            <div className="modal-body">
-              {selectedItem && (
-                <div>
-                  <img
-                    className="img-fluid"
-                    src={selectedItem.img}
-                    alt={selectedItem.title}
-                  />
-                  <p className="mt-3">{selectedItem.techno}</p>
-                  <a
-                    href={selectedItem.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-primary"
-                  >
-                    Voir le projet
-                  </a>
+              {!loading && !error && (
+                <div className="row">
+                  {filteredData.map((item) => (
+                    <div
+                      key={item.id}
+                      className="col-lg-4 col-sm-6 mb-4"
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                    >
+                      <div className="portfolio-item">
+                        <a className="portfolio-link" href="#">
+                          <div className="portfolio-hover">
+                            <div className="portfolio-hover-content">
+                              <a
+                                href="#"
+                                data-bs-toggle="modal"
+                                data-bs-target="#exampleModal"
+                                onClick={() => setSelectedItem(item)}
+                              >
+                                <i className="bi bi-plus fa-3x"></i>
+                              </a>
+                              <a
+                                href={item.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                <i className="bi bi-link-45deg fa-2x"></i>
+                              </a>
+                            </div>
+                          </div>
+                          <img
+                            className="img-fluid img-portfolio"
+                            src={item.img}
+                            alt={item.title}
+                          />
+                        </a>
+                        <div className="portfolio-caption">
+                          <div className="portfolio-caption-heading">
+                            {item.title}
+                          </div>
+                          <div className="portfolio-caption-subheading text-muted">
+                            {item.techno}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               )}
             </div>
           </div>
+        </section>
+
+        {/* Modal */}
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-fullscreen">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  {selectedItem?.title || "Détails"}
+                </h5>
+                <a
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <i className="bi bi-x-lg"></i>
+                </a>
+              </div>
+              <div className="modal-body">
+                {selectedItem && (
+                  <div>
+                    <img
+                      className="img-fluid"
+                      src={selectedItem.img}
+                      alt={selectedItem.title}
+                    />
+                    <p className="mt-3">{selectedItem.techno}</p>
+                    <a
+                      href={selectedItem.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-primary"
+                    >
+                      Voir le projet
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
