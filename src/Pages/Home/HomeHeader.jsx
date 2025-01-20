ef0cc88fcb89f3cf5c897b9b4116e875519ab8cb
santatramcd <andrianjakasantatra22@gmail.com>
2025-01-20 12:31:02 +0300

    animate fadeInUp btn download cv

--- src/Pages/Home/HomeHeader.jsx
+++ src/Pages/Home/HomeHeader.jsx
@@ -3,15 +3,13 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <header className="masthead" id="page-top">
+      <header className="masthead " id="page-top">
         <div className="container">
           <div className="masthead-subheading">Je Suis Santatra Michado</div>
-          <div className="masthead-heading text-uppercase">
-            Web Developer
-          </div>
+          <div className="masthead-heading text-uppercase">Web Developer</div>
           {/* Remplacez "cv-santatra.pdf" par le nom de votre fichier */}
           <a
-            className="btn btn-primary btn-xl text-uppercase"
+            className="btn btn-primary btn-xl text-uppercase animated animatedFadeInUp  fadeInUp"
             href="/cv-santatra.pdf"
             download="Santatra-Michado-CV.pdf"
           >
