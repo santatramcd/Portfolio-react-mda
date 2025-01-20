036a744d95f6cc5a16ddfe62ff266e728816f298
santatramcd <andrianjakasantatra22@gmail.com>
2025-01-20 12:52:57 +0300

    change the logos and url

--- src/Pages/Home/Sixsec.jsx
+++ src/Pages/Home/Sixsec.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logos1 from '../../assets/logos/facebook.svg';
-import logos2 from '../../assets/logos/google.svg';
-import logos3 from '../../assets/logos/ibm.svg';
-import logos4 from '../../assets/logos/microsoft.svg';
+// import logos1 from '../../assets/logos/facebook.svg';
+// import logos2 from '../../assets/logos/google.svg';
+// import logos3 from '../../assets/logos/ibm.svg';
+// import logos4 from '../../assets/logos/microsoft.svg';
 
+import logos1 from '../../assets/logos/createak-logo.png';
+import logos2 from '../../assets/logos/mda.png';
+import logos3 from '../../assets/logos/rr.webp';
+import logos4 from '../../assets/logos/logo.png';
 export default function About() {
   return (
     <div>
         <div className="py-5">
             <div className="container">
                 <div className="row align-items-center">
-                    <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={logos1} alt="..." aria-label="Microsoft Logo" /></a>
+                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                        <a href="https://createak.mu/"><img className="img-fluid img-brand d-block mx-auto" src={logos1} alt="..." aria-label="createak Logo" /></a>
                     </div>
-                    <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={logos2} alt="..." aria-label="Google Logo" /></a>
+                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                        <a href="https://mada-digital-agency.vercel.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos2} alt="..." aria-label="mda Logo" /></a>
                     </div>
-                    <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="Facebook Logo" /></a>
+                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                        <a href="https://robinruth-thailand.com/"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="robin ruth Logo" /></a>
                     </div>
-                    <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={logos4} alt="..." aria-label="IBM Logo" /></a>
+                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                        <a href="https://maki-car-rental-mada.netlify.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos4} alt="..." aria-label="maki Logo" /></a>
                     </div>
                 </div>
             </div>
