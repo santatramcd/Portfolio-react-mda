d3361c365721199515aa7ee471df41337077c16e
santatramcd <andrianjakasantatra22@gmail.com>
2025-02-07 11:41:06 +0300

    add animation preloader

--- src/display/Footer.jsx
+++ src/display/Footer.jsx
@@ -1,51 +1,76 @@
 import React from "react";
 import { useState, useEffect } from "react";
 export default function Footer() {
-    const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    // Fonction pour gérer le scroll
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 200) {
-          setIsVisible(true);
-        } else {
-          setIsVisible(false);
-        }
-      };
-  
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-  
-    // Fonction pour remonter en haut
-    const scrollToTop = () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
+  // Fonction pour gérer le scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
     };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // Fonction pour remonter en haut
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <div className="container">
-     <footer className="footer py-4">
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-lg-4 text-lg-start">Copyright &copy; Santatra Michado 2025</div>
-                    <div className="col-lg-4 my-3 my-lg-0">
-                        <a className="btn btn-dark btn-social mx-2" href="https://github.com/santatramcd" aria-label="Twitter"><i className="bi bi-github"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="https://www.facebook.com/profile.php?id=100010304222710" aria-label="Facebook"><i className="bi bi-facebook"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i className="bi bi-linkedin"></i></a>
-                    </div>
-                    <div className="col-lg-4 text-lg-end">
-                        <a className="nav-link text-decoration-none number-phone" href="#!">+261 32 46 658 49</a>
-                    </div>
-                </div>
+      <footer className="footer py-4">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-4 text-lg-start">
+              Copyright &copy; Santatra Michado 2025
             </div>
-        </footer>
-        <div
-      className={`div-fixed ${isVisible ? "show" : "hide"}`}
-      onClick={scrollToTop}
-    >
-      <a href="#">
-        <i className="bi bi-chevron-up"></i>
-      </a>
-    </div>
+            <div className="col-lg-4 my-3 my-lg-0">
+              <a
+                className="btn btn-dark btn-social mx-2"
+                href="https://github.com/santatramcd"
+                aria-label="Twitter"
+              >
+                <i className="bi bi-github"></i>
+              </a>
+              <a
+                className="btn btn-dark btn-social mx-2"
+                href="https://www.facebook.com/profile.php?id=100010304222710"
+                aria-label="Facebook"
+              >
+                <i className="bi bi-facebook"></i>
+              </a>
+              <a
+                className="btn btn-dark btn-social mx-2"
+                href="#!"
+                aria-label="LinkedIn"
+              >
+                <i className="bi bi-linkedin"></i>
+              </a>
+            </div>
+            <div className="col-lg-4 text-lg-end">
+              <a
+                className="nav-link text-decoration-none number-phone"
+                href="#!"
+              >
+                +261 32 46 658 49
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div
+        className={`div-fixed ${isVisible ? "show" : "hide"}`}
+        onClick={scrollToTop}
+      >
+        <a href="#">
+          <i className="bi bi-chevron-up"></i>
+        </a>
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./App.scss"
 import App from "./App.jsx";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
