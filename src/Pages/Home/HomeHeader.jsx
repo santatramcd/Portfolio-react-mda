7b492709e1b55de74ce24a52fbb7d6f7ed5b402c
santatramcd <andrianjakasantatra22@gmail.com>
2025-02-11 12:06:22 +0300

    update font family

--- src/Pages/Home/HomeHeader.jsx
+++ src/Pages/Home/HomeHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import banniere from "../../assets/bannière.jpg";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 export default function About() {
   const [rotatedText, setRotatedText] = useState("");
@@ -24,19 +24,24 @@ export default function About() {
       <header className="masthead " id="page-top">
         <div className="container">
           <div className="row row-bann animated animatedFadeInUp fadeInUp">
-            <div className="col-lg-6" >
+            <div className="col-lg-6">
+              <div className="div-codes">
+                <span>
+                  {"{"}#{"}"}
+                </span>
+              </div>
               <div className="masthead-subheading">
-              {t('Iam')} Santatra Michado
+                {"{"}" <span>_{t("Iam")} Santatra Michado</span> "{"}"}
               </div>
               <div className="masthead-heading text-uppercase">
-              {t('dev')} <span className="name-sm"> Front-End</span>
+                {t("dev")} <span className="name-sm"> Front-End</span>
               </div>
               <a
                 className="btn btn-primary btn-xl text-uppercase animated animatedFadeInUp fadeInUp"
                 href="/cv-santatra.pdf"
                 download="Santatra-Michado-CV.pdf"
               >
-                {t('down')}
+                {t("down")}
               </a>
             </div>
             <div className="col-lg-6">
@@ -47,11 +52,7 @@ export default function About() {
                     dangerouslySetInnerHTML={{ __html: rotatedText }}
                   />
                   <div className="div-banners">
-                    <img
-                      src={banniere}
-                      alt="image"
-                      className="im-banners"
-                    />
+                    <img src={banniere} alt="image" className="im-banners" />
                   </div>
                 </div>
               </div>
