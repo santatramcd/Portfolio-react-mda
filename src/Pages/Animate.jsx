1258a495aa9636ec6336764e2d0a395bd7e11d5e
santatramcd <andrianjakasantatra22@gmail.com>
2025-03-28 12:21:41 +0300

    add a new animate loading

--- src/Pages/Animate.jsx
+++ src/Pages/Animate.jsx
@@ -1,23 +1,39 @@
 import React, { useEffect } from "react";
+import gsap from "gsap"; // Assurez-vous d'importer gsap
 
 export default function Animate() {
   useEffect(() => {
-    const preloader = document.querySelector("#preloader");
-    if (preloader) {
-      setTimeout(() => {
-        preloader.classList.add("loaded");
-      }, 1000);
-      setTimeout(() => {
-        preloader.remove();
-      }, 2000);
+    function startLoader() {
+      let bars = document.querySelectorAll(".bar");
+
+      if (bars.length === 0) {
+        console.error("Aucun élément .bar trouvé !");
+        return;
+      }
+
+      gsap.fromTo(
+        ".bar",
+        { height: "100vh" },
+        {
+          duration: 1.5,
+          height: 0,
+          stagger: { amount: 2 },
+          ease: "power4.out",
+        }
+      );
     }
+
+    setTimeout(startLoader);
   }, []);
 
   return (
-    <div>
-      <div id="preloader">
-        <div className="line"></div>
-      </div>
+    <div className="overlays">
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
     </div>
   );
 }
