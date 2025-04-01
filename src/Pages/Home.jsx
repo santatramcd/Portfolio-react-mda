022af546d8c283315e685b8511ec2fa246756590
santatramcd <andrianjakasantatra22@gmail.com>
2025-04-01 09:57:34 +0300

    add chatbox

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,6 +8,7 @@ import Cinqsec from "./Home/cinqsec";
 import Sixsec from "./Home/Sixsec";
 import Skills from "./Home/skills";
 import Septsec from "./Home/Septsec";
+import Chatbox from "./Home/chatbox";
 // import Gsap from "./Home/gsap"
 export default function Home() {
   return (
@@ -23,6 +24,7 @@ export default function Home() {
           <Skills />
           <Sixsec />
           <Septsec />
+          <Chatbox/>
         </div>
       </main>
     </>
---
+++ src/Pages/Home/chatbox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const chatbox = () => {
+  return (
+    <div>
+      <div
+        className="elfsight-app-1a89da90-874c-40c7-88aa-1529b41d8435"
+        data-elfsight-app-lazy
+      ></div>
+    </div>
+  );
+};
+
+export default chatbox;
