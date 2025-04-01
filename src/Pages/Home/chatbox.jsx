2507ba0c800bf42652050844d9577408a9723b23
santatramcd <andrianjakasantatra22@gmail.com>
2025-04-01 10:30:20 +0300

    add chatbox

--- src/Pages/Home/chatbox.jsx
+++ src/Pages/Home/chatbox.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const chatbox = () => {
   return (
     <div>
-      <div
-        className="elfsight-app-1a89da90-874c-40c7-88aa-1529b41d8435"
-        data-elfsight-app-lazy
-      ></div>
+      <div className="elfsight-app-5c037acc-4d00-4566-9a1e-a6b594c2681e" data-elfsight-app-lazy></div>
     </div>
   );
 };
