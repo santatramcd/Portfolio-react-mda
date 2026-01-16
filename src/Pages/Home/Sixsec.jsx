725179d76262d7ed3be6002340207cf258cd926d
Santatra Michado <andrianjakasantatra22@gmail.com>
2026-01-16 14:30:38 +0300

    add new logo

--- src/Pages/Home/Sixsec.jsx
+++ src/Pages/Home/Sixsec.jsx
@@ -15,17 +15,23 @@ export default function About() {
         <div className="py-5">
             <div className="container">
                 <div className="row align-items-center">
-                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                    <div className="col-md-2 col-sm-6 my-3 card-logos">
                         <a href="https://createak.mu/"><img className="img-fluid img-brand d-block mx-auto" src={logos1} alt="..." aria-label="createak Logo" /></a>
                     </div>
-                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                    <div className="col-md-2 col-sm-6 my-3 card-logos">
                         <a href="https://mada-digital-agency.vercel.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos2} alt="..." aria-label="mda Logo" /></a>
                     </div>
-                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                    <div className="col-md-2 col-sm-6 my-3 card-logos">
                         <a href="https://robinruth-thailand.com/"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="robin ruth Logo" /></a>
                     </div>
-                    <div className="col-md-3 col-sm-6 my-3 card-logos">
+                    <div className="col-md-2 col-sm-6 my-3 card-logos">
                         <a href="https://maki-car-rental-mada.netlify.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos4} alt="..." aria-label="maki Logo" /></a>
+                    </div>
+                    <div className="col-md-2 col-sm-6 my-3 card-logos">
+                        <a href="https://olyra-digital.com"><img className="img-fluid img-brand d-block mx-auto" src="https://olyra-digital.com/wp-content/uploads/2023/11/Logo-Hotizontale-t@3x-scaled.png" alt="..." aria-label="olyra Logo" /></a>
+                    </div>
+                    <div className="col-md-2 col-sm-6 my-3 card-logos">
+                        <a href="https://tsb-energie.fr"><img className="img-fluid img-brand d-block mx-auto tsb-logo" src="https://tsb-energie.fr/wp-content/uploads/2025/10/cropped-Pico-TSBt1@3x-scaled-1.png" alt="..." aria-label="tsb Logo" /></a>
                     </div>
                 </div>
             </div>
