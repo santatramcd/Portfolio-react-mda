0a089257b45b49db891ae413e2cc014a8d6e21a5
Santatra Michado <andrianjakasantatra22@gmail.com>
2026-01-20 13:37:26 +0300

    add new years

--- src/display/Footer.jsx
+++ src/display/Footer.jsx
@@ -27,7 +27,7 @@ export default function Footer() {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-lg-4 text-lg-start">
-              Copyright &copy; Santatra Michado 2026
+                Copyright &copy; Santatra Michado {new Date().getFullYear()}
             </div>
             <div className="col-lg-4 my-3 my-lg-0">
               <a
