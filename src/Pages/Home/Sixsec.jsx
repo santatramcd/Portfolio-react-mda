85a0095c8fd22d798a8bd86b22a5644a25561548
Santatra Michado <andrianjakasantatra22@gmail.com>
2026-03-06 13:20:39 +0300

    update design

--- src/Pages/Home/Sixsec.jsx
+++ src/Pages/Home/Sixsec.jsx
@@ -13,35 +13,35 @@ import logos4 from '../../assets/logos/logo.png';
 import { useTranslation } from "react-i18next";
 
 export default function About() {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div>
+    return (
         <div className="py-5">
-            <div className="container">
-                <h2 className="section-heading text-uppercase text-center">{t("partenaire")} :</h2>
-                <div className="row align-items-center">
-                    <div className="col-md-2 col-sm-6 my-3 card-logos">
-                        <a href="https://createak.mu/"><img className="img-fluid img-brand d-block mx-auto" src={logos1} alt="..." aria-label="createak Logo" /></a>
-                    </div>
-                    <div className="col-md-2 col-sm-6 my-3 card-logos">
-                        <a href="https://mada-digital-agency.vercel.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos2} alt="..." aria-label="mda Logo" /></a>
-                    </div>
-                    <div className="col-md-2 col-sm-6 my-3 card-logos">
-                        <a href="https://robinruth-thailand.com/"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="robin ruth Logo" /></a>
-                    </div>
-                    <div className="col-md-2 col-sm-6 my-3 card-logos">
-                        <a href="https://maki-car-rental-mada.netlify.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos4} alt="..." aria-label="maki Logo" /></a>
-                    </div>
-                    <div className="col-md-2 col-sm-6 my-3 card-logos">
-                        <a href="https://olyra-digital.com"><img className="img-fluid img-brand d-block mx-auto" src="https://olyra-digital.com/wp-content/uploads/2023/11/Logo-Hotizontale-t@3x-scaled.png" alt="..." aria-label="olyra Logo" /></a>
-                    </div>
-                    <div className="col-md-2 col-sm-6 my-3 card-logos">
-                        <a href="https://tsb-energie.fr"><img className="img-fluid img-brand d-block mx-auto tsb-logo" src="https://tsb-energie.fr/wp-content/uploads/2025/10/cropped-Pico-TSBt1@3x-scaled-1.png" alt="..." aria-label="tsb Logo" /></a>
-                    </div>
+            <h2 className="section-heading text-uppercase text-center">{t("partenaire")} :</h2>
+            <div class="marquee-wrap mt-5">
+                <div class="marquee">
+                    <span class="marquee-item"><a href="https://ova-renov.fr/"><img className="img-fluid img-brand d-block mx-auto" src="https://ova-renov.fr/wp-content/uploads/2025/11/Logo1-Transparent-blanc@3x-768x423.png" alt="..." aria-label="ova Logo" /></a></span>
+                    <span class="marquee-item"><a href="https://olyra-digital.com"><img className="img-fluid img-brand d-block mx-auto" src="https://olyra-digital.com/wp-content/uploads/2023/11/Logo-Hotizontale-t@3x-scaled.png" alt="..." aria-label="olyra Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://createak.mu/"><img className="img-fluid img-brand d-block mx-auto" src={logos1} alt="..." aria-label="createak Logo" /></a>
+                    </span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://mada-digital-agency.vercel.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos2} alt="..." aria-label="mda Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://robinruth-thailand.com/"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="robin ruth Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://maki-car-rental-mada.netlify.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos4} alt="..." aria-label="maki Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://olyra-digital.com"><img className="img-fluid img-brand d-block mx-auto" src="https://olyra-digital.com/wp-content/uploads/2023/11/Logo-Hotizontale-t@3x-scaled.png" alt="..." aria-label="olyra Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://robinruth-thailand.com/"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="robin ruth Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://tsb-energie.fr"><img className="img-fluid img-brand d-block mx-auto tsb-logo" src="https://tsb-energie.fr/wp-content/uploads/2025/10/cropped-Pico-TSBt1@3x-scaled-1.png" alt="..." aria-label="tsb Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://ova-renov.fr/"><img className="img-fluid img-brand d-block mx-auto" src="https://ova-renov.fr/wp-content/uploads/2025/11/Logo1-Transparent-blanc@3x-768x423.png" alt="..." aria-label="ova Logo" /></a></span>
+                    <span class="marquee-item"><a href="https://createak.mu/"><img className="img-fluid img-brand d-block mx-auto" src={logos1} alt="..." aria-label="createak Logo" /></a>
+                    </span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://olyra-digital.com"><img className="img-fluid img-brand d-block mx-auto" src="https://olyra-digital.com/wp-content/uploads/2023/11/Logo-Hotizontale-t@3x-scaled.png" alt="..." aria-label="olyra Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://mada-digital-agency.vercel.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos2} alt="..." aria-label="mda Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://robinruth-thailand.com/"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="robin ruth Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://maki-car-rental-mada.netlify.app/"><img className="img-fluid img-brand d-block mx-auto" src={logos4} alt="..." aria-label="maki Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://olyra-digital.com"><img className="img-fluid img-brand d-block mx-auto" src="https://olyra-digital.com/wp-content/uploads/2023/11/Logo-Hotizontale-t@3x-scaled.png" alt="..." aria-label="olyra Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://tsb-energie.fr"><img className="img-fluid img-brand d-block mx-auto tsb-logo" src="https://tsb-energie.fr/wp-content/uploads/2025/10/cropped-Pico-TSBt1@3x-scaled-1.png" alt="..." aria-label="tsb Logo" /></a></span><span class="marquee-dot">◆</span>
+                    <span class="marquee-item"><a href="https://robinruth-thailand.com/"><img className="img-fluid img-brand d-block mx-auto" src={logos3} alt="..." aria-label="robin ruth Logo" /></a></span><span class="marquee-dot">◆</span>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
